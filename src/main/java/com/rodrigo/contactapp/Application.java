42237f27a1272616e939e8bb37ff4d2351ac545a
Rodrigo <romacas@hotmail.co.uk>
2017-03-05 13:20:58 +0000

    Create SessionFactory

--- src/main/java/com/rodrigo/contactapp/Application.java
+++ src/main/java/com/rodrigo/contactapp/Application.java
@@ -1,9 +1,22 @@
 package com.rodrigo.contactapp;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
 /**
  * Created by Rodrigo on 05/03/2017.
  */
 public class Application {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
 
     public static void main(String[] args) {
 
